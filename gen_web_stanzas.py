656f97707516802055aa6f49c4418c725eb1f97e
CruizK <Cruiz.Kusman@gmail.com>
2020-10-31 13:30:36 -0500

    Modified regex; Almost ready to be unleashed just needs to properly cache and exit on request failure as well as going through links 5 ot 10 at a time, saving them and then moving on the next

--- gen_web_stanzas.py
+++ gen_web_stanzas.py
@@ -5,60 +5,89 @@ from main import get_stanzas
 from datetime import datetime
 from multiprocessing import Pool
 import os.path
-import json 
+import json
 
 json_file = "complete_stanzas.json"
 
-def check_file():
-  if os.path.exists(json_file):
-    with open(json_file, "r") as f:
-      data = json.load(f)
-      #for d in data:
-      #  data[d][0] = datetime.fromisoformat(data[d][0])
-    return data
-  else:
-    return None
 
-def cube(x):
-  return x**3
+def check_file():
+    if os.path.exists(json_file):
+        with open(json_file, "r") as f:
+            data = json.load(f)
+            for d in data:
+                try:
+                    d['last_updated'] = datetime.fromisoformat(data['last_updated'])
+                except:
+                    pass
+            return data
+    else:
+        with open(json_file, "w") as f:
+            web_stanzas = get_stanzas()
+            links = {web_stanzas[key][1]: {'web_title': key}
+                     for key in web_stanzas}
+            json.dump(links, f, indent=4)
+            return links
+
 
 def gen_web_stanzas():
-    web_stanzas = get_stanzas()
-    links = [web_stanzas[key][1] for key in web_stanzas] # lol
-    subset_links = links[5:10]
-    stanzas = check_file()
-    with Pool(10) as p:
-      results = [p.apply_async(parsePage, args=(i,)) for i in subset_links]
-      arr = [result.get() for result in results]
-      with open(json_file, 'w') as f:
-        json.dump(arr, f, indent=4)
-    """
-    for key in subset_keys:
-        link = web_stanzas[key][1]
-        try:
-          parsePage(link)
-        except:
-          print("Error getting site")
-"""
+    stanza_data = check_file()
+
+    not_updated_subset = []
+    for key in stanza_data:
+        if 'last_updated' not in stanza_data[key]:
+            not_updated_subset.append(key)
+
+    
+    with Pool(5) as p:
+        results = [p.apply_async(parsePage, args=(i,)) for i in not_updated_subset[:5]]
+        arr = [result.get() for result in results]
+        for i in arr:
+            if i['link'] in stanza_data:
+                stanza_data[i['link']].update(i)
+        
+        with open(json_file, "w") as f:
+            json.dump(stanza_data, f, indent=4)
 
 
 def parsePage(link):
-  page = requests.get(link)
-  soup = BeautifulSoup(page.text, 'html.parser')
-  pre_tags = soup.find_all('pre')
-  for tag in pre_tags:
-      lines = tag.text.split('\n')
-      for line in lines:
-          if re.match(r'^Title (.+)$', line):
-              print(line)
-              search = re.search(r'^Title ((?:(?! \(updated).)*) \(updated (\d{8})\)', line) # Wow so beautiful
-              title = search.group(1).strip() 
-              last_updated = datetime.strptime(search.group(2), "%Y%m%d")
-              return {
-                'title': title,
-                'last_updated': str(last_updated),
-                'stanza_text': tag.text
-              }
+    page = requests.get(link)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    pre_tags = soup.find_all('pre')
+    
+    main_stanza_text = ""
+    other_text = []
+    last_updated = ""
+    title = ""
+
+    for tag in pre_tags:
+        lines = tag.text.split('\n')
+        is_title_tag = False
+        for line in lines:
+            if re.match(r'^Title (.+)$', line):
+                is_title_tag = True
+                print(line)
+                search = re.search(r'^Title ((?:(?! \(updated).)*) \(updated (\d{8})\)', line)  # Wow so beautiful
+
+                if search == None: # Make the assumption that it has no (updated) format ex: Title 123Library
+                    search = re.search(r'^Title (.+)$', line)
+                title = search.group(1).strip()
+
+                last_updated = ""
+                main_stanza_text = tag.text
+                try:
+                    last_updated = datetime.strptime(search.group(2), "%Y%m%d")
+                except:
+                    last_updated = "No Data"
+        if is_title_tag == False:
+            other_text.append(str(tag.text))
+
+    return {
+        'title': title,
+        'last_updated': str(last_updated),
+        'main_stanza_text': main_stanza_text,
+        'other_text': other_text,
+        'link': link
+    }
 
 
 if __name__ == "__main__":
